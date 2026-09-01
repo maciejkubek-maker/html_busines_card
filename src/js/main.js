3bece032c6f30ba954cbae399970a40a4ce5a5a7
Maciej <adamabler@gmail.com>
2026-09-01 15:50:51 +0200

    update data, add projects data and new template projects.html + projects.css, add photo to projects

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Obsługa menu mobilnego
     const hamburgerBtn = document.getElementById('hamburgerBtn');
     const mobileMenu = document.getElementById('mobileMenu');
     
@@ -14,4 +15,27 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
+
+    // Obsługa modalu powiększającego zdjęcia w projektach
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImg");
+    const images = document.querySelectorAll(".project-img-card img");
+
+    if (modal && modalImg && images.length > 0) {
+        images.forEach(img => {
+            img.style.cursor = "pointer";
+            img.addEventListener("click", () => {
+                modal.style.display = "block";
+                modalImg.src = img.src;
+            });
+        });
+    }
 });
+
+// Globalna funkcja zamykania okna modalnego
+function closeModal() {
+    const modal = document.getElementById("imageModal");
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
